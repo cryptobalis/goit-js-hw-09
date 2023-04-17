1f82e733e702adf16e22b067272edd7cf72deff7
Maksym Yeroshevych <makserosh95@gmail.com>
2023-04-17 23:12:04 +0300

    fixed bugs

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,6 +4,12 @@ import "notiflix/dist/notiflix-3.2.6.min.css";
 
 
 function createPromise(position, delay) {
+
+    // очищення форми
+    form.delay.value = '';
+    form.step.value = '';
+    form.amount.value = '';
+    
   return new Promise((resolve, reject) => {
   setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
@@ -15,19 +21,32 @@ function createPromise(position, delay) {
   Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
   }, delay);
+  
   });
+  
   }
 
-
 const form = document.querySelector('.form');
+// const button = form.querySelector('button');
+
+
 form.addEventListener('submit', event => {
   event.preventDefault();
+ 
   const delay = parseInt(form.delay.value);
   const step = parseInt(form.step.value);
   const amount = parseInt(form.amount.value);
+
+  // button.disabled = true;
+
   for (let i = 1; i <= amount; i++) {
+
     const currentDelay = delay + (i - 1) * step;
     createPromise(i, currentDelay);
   }
+
 });
+
+
+
 
